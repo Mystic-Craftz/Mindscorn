01f5f04f9f26e8e1f51d08b699e3df33b3ebe74b
Muhammad Usman Khilji <manian1221@gmail.com>
2025-09-18 20:36:37 +0500

    Parasites done with audio and saving

--- Assets/_Scripts/AI Enemies/Parasite/ParasiteBody.cs
+++ Assets/_Scripts/AI Enemies/Parasite/ParasiteBody.cs
@@ -1,16 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
 public class ParasiteBody : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Animator bodyAnimator;
+    [SerializeField] private Parasite parasite;
+    [SerializeField] private float startingDelay = 0f;
+    [SerializeField] private float durationBeforeGettingOut = 2f;
+
+    private const string CAN_SWITCH = "CanSwitch";
+    private const string BODY_SHAKING_1 = "BodyShaking1";
+
+    private void Start()
     {
-        
+        bodyAnimator.SetBool(CAN_SWITCH, false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TriggerParasiteGettingOut()
     {
-        
+        StartCoroutine(ParasiteGettingOut());
+    }
+
+    private IEnumerator ParasiteGettingOut()
+    {
+        yield return new WaitForSeconds(startingDelay);
+        bodyAnimator.CrossFade(BODY_SHAKING_1, 0.1f);
+        yield return new WaitForSeconds(durationBeforeGettingOut);
+        bodyAnimator.SetTrigger(CAN_SWITCH);
+        parasite.GetOut();
     }
 }
