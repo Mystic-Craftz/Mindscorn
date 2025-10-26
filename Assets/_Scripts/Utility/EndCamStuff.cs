8471c182c24034eee4dfbb32c097dbe21a5aff0b
Areesh Ahmad <94343622+Picar0@users.noreply.github.com>
2025-10-26 23:39:05 +0500

    Lights And grocery

--- Assets/_Scripts/Utility/EndCamStuff.cs
+++ Assets/_Scripts/Utility/EndCamStuff.cs
@@ -1,12 +1,18 @@
 using System.Collections;
+using DG.Tweening;
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class EndCamStuff : MonoBehaviour
 {
     [SerializeField] private Camera mainCam;
     [SerializeField] private CinemachineCamera cam;
     [SerializeField] private float delay = 1f;
     [SerializeField] private int blinks = 3;
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float duration = 1f;
 
     public void SetCamBlend()
     {
@@ -41,4 +47,32 @@ public class EndCamStuff : MonoBehaviour
     {
         cam.Priority = 100;
     }
+
+    public void StopMovement()
+    {
+        PlayerController.Instance.SetCanMove(false);
+        EscapeMenuUI.Instance.DisableToggle();
+        InventoryManager.Instance.DisableToggle();
+    }
+
+    public void FadeToBlack()
+    {
+        if (fadeImage != null)
+        {
+            fadeImage.gameObject.SetActive(true);
+            fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, 0f);
+            fadeImage.DOFade(1f, duration)
+                .SetEase(Ease.Linear)
+                .OnComplete(() =>
+                {
+                    LoadCreditsScene();
+                });
+        }
+    }
+
+    private void LoadCreditsScene()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
 }
